71778cdecac01d3babfaf433c96fe46d38afdbdc
GullapalliAkhil <Akhil.Gullapalli@outlook.com>
2019-11-15 16:42:51 +0530

    Adding the Syntax checker.

--- Code/ai/Src/main/browser.py
+++ Code/ai/Src/main/browser.py
@@ -9,8 +9,17 @@ class BrowserCall:
     def call_browser():
         with open(r"C:\Users\AKHIL\Desktop\CODE-AI\Code\ai\Src\main\config.json", "r") as read_file:
             data = json.load(read_file)
-            p = subprocess.Popen(["powershell.exe", f"start-process -FilePath 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' -ArgumentList '{data['urlToHit']}' "])
-            p.communicate()
+            typeOfBrowser = data['browserName']
+            if typeOfBrowser.upper() in "CHROME":
+                p = subprocess.Popen(["powershell.exe",
+                                      f"start-process -FilePath 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' -ArgumentList '{data['urlToHit']}' "])
+                p.communicate()
+            elif typeOfBrowser.upper() in "FIREFOX":
+                p = subprocess.Popen(["powershell.exe", f"start-process -FilePath 'C:\\Program Files\\Mozilla "
+                                                        f"Firefox\\firefox.exe' -ArgumentList '{data['urlToHit']}' "])
+                p.communicate()
+            else:
+                print("Please give the value which is supported by us.")
 
 
 if __name__ == '__main__':
--- Code/ai/Src/main/run.py
+++ Code/ai/Src/main/run.py
@@ -1,4 +1,5 @@
 from Code.ai.Src.main.browser import BrowserCall
+from Code.ai.Src.main.check import Check
 from Code.ai.Src.main.createFile import CreateCode
 
 
@@ -7,8 +8,8 @@ class Run:
     def run_files():
         CreateCode.create_file()  # Create the Java or Python Files.
         BrowserCall.call_browser()  # Open browser through power shell.
+        Check.syntax_validation()  # Syntax Validations
 
 
 if __name__ == '__main__':
     Run.run_files()
-
