0a3cbead0b470a1ed79359b857b3cfe1d97c1852
Gullapalli Akhil Sai <52671728+GullapalliAkhil@users.noreply.github.com>
2020-03-22 00:08:56 +0530

    Removing the json file and added the config.py file and class variables to use.
    All the class variables in the config.py file helps us to provide the configuration values which are neccessary to create the file and to write the code.

--- Code/ai/Src/main/browser.py
+++ Code/ai/Src/main/browser.py
@@ -1,25 +1,34 @@
 import subprocess
 import json
+import webbrowser
 
-import self as self
+from Code.ai.Src.main.Config import configurations_to_run
 
 
 class BrowserCall:
     @staticmethod
     def call_browser():
-        with open(r"..\CODE-AI\Code\ai\Src\main\config.json", "r") as read_file:
-            data = json.load(read_file)
-            typeOfBrowser = data['browserName']
-            if typeOfBrowser.upper() in "CHROME":
-                p = subprocess.Popen(["powershell.exe",
-                                      f"start-process -FilePath 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' -ArgumentList '{data['urlToHit']}' "])
-                p.communicate()
-            elif typeOfBrowser.upper() in "FIREFOX":
-                p = subprocess.Popen(["powershell.exe", f"start-process -FilePath 'C:\\Program Files\\Mozilla "
-                                                        f"Firefox\\firefox.exe' -ArgumentList '{data['urlToHit']}' "])
-                p.communicate()
+        typeOfBrowser = configurations_to_run.browserName
+        product_url = configurations_to_run.urlToHit
+        if typeOfBrowser.upper() in "CHROME":
+            if configurations_to_run.Operating_System == 'MacOS':
+                chrome_path = 'open -a /Applications/Google\\ Chrome.app %s'
+                webbrowser.get(chrome_path).open(product_url)
+            elif configurations_to_run.Operating_System == 'Windows':
+                chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+                webbrowser.get(chrome_path).open(product_url)
+            elif configurations_to_run.Operating_System == 'Linux':
+                chrome_path = '/usr/bin/google-chrome %s'
+                webbrowser.get(chrome_path).open(product_url)
             else:
-                print("Please give the value which is supported by us.")
+                print("We didn't support for the operating system you had.")
+
+        elif typeOfBrowser.upper() in "FIREFOX":
+            p = subprocess.Popen(["powershell.exe", f"start-process -FilePath 'C:\\Program Files\\Mozilla "
+                                                    f"Firefox\\firefox.exe' -ArgumentList '{data['urlToHit']}' "])
+            p.communicate()
+        else:
+            print("Please give the value which is supported by us.")
 
 
 if __name__ == '__main__':
--- Code/ai/Src/main/createFile.py
+++ Code/ai/Src/main/createFile.py
@@ -2,6 +2,8 @@ import os
 import json
 import requests
 
+from Code.ai.Src.main.Config import configurations_to_run
+
 """This class helps us to create the required directory and files. A file named main.java will be created with a 
 class name and main method. In that main method depends up on the user inputs code will be get created automatically. 
 if user selects the browser as chrome, code which helps to launch the chrome browser will be build automatically in 
@@ -13,46 +15,43 @@ Currently for now we are only supporting the chrome and firefox browsers."""
 class CreateCode:
     @staticmethod
     def create_file():
-
-        with open(r"C:\Users\AKHIL\Desktop\CODE-AI\Code\ai\Src\main\config.json", "r") as read_file:
-            data = json.load(read_file)
-            path = data['pathName']  # For example /Project/Automaton
+        path = configurations_to_run.pathName  # For example /Project/Automaton
+        try:
+            os.mkdir(path)
+        except OSError:
+            print("Creation of the directory %s failed" % path)
+        else:
+            print("Successfully created the directory %s " % path)
+        # file creation
+        typeOfFile = configurations_to_run.LanguageCode
+        if typeOfFile in "java":
+            filePath = os.path.join(path, 'main.java')
+            f = open(filePath, 'a+')
+            productName = configurations_to_run.className
+            f.write(f'class {productName}\n')
+            f.write("{")
+            f.write("\n public static void main(String args[]){\n")
+            typeOfBrowser = configurations_to_run.browserName
+            if typeOfBrowser.upper() in "CHROME":
+                chromeDriverPath = configurations_to_run.chrome_driver_loc
+                f.write(f'System.setProperty("webdriver.chrome.driver","{chromeDriverPath}");')
+                f.write("\n WebDriver driver = new ChromeDriver();\n")
+            elif typeOfBrowser.upper() in "FIREFOX":
+                f.write("WebDriver driver = new FirefoxDriver();\n")
+            productURL = configurations_to_run.urlToHit
             try:
-                os.mkdir(path)
-            except OSError:
-                print("Creation of the directory %s failed" % path)
-            else:
-                print("Successfully created the directory %s " % path)
-            # file creation
-            typeOfFile = data['LanguageCode']
-            if typeOfFile in "java":
-                filePath = os.path.join(path, 'main.java')
-                f = open(filePath, 'a+')
-                productName = data['className']
-                f.write(f'class {productName}\n')
-                f.write("{")
-                f.write("\n public static void main(String args[]){\n")
-                typeOfBrowser = data['browserName']
-                if typeOfBrowser.upper() in "CHROME":
-                    chromeDriverPath = data['browserDriverLoc']
-                    f.write(f'System.setProperty("webdriver.chrome.driver","{chromeDriverPath}");')
-                    f.write("\n WebDriver driver = new ChromeDriver();\n")
-                elif typeOfBrowser.upper() in "FIREFOX":
-                    f.write("WebDriver driver = new FirefoxDriver();\n")
-                productURL = data['urlToHit']
-                try:
-                    r = requests.head(productURL)
-                    f.write(f'driver.get("{productURL}");\n')
-                except requests.ConnectionError:
-                    print("Connection Error, Your API is not able to make a contact with your server")
-                    f.write("/*NOTE: - Connection Error, Your API is not able to make a contact with your server.*/\n")
-                f.write("}\n")
-                f.write("}")
-                print("A java file with the above parameters successfully")
-                f.close()
+                r = requests.head(productURL)
+                f.write(f'driver.get("{productURL}");\n')
+            except requests.ConnectionError:
+                print("Connection Error, Your API is not able to make a contact with your server")
+                f.write("/*NOTE: - Connection Error, Your API is not able to make a contact with your server.*/\n")
+            f.write("}\n")
+            f.write("}")
+            print("A java file with the above parameters successfully")
+            f.close()
 
-            else:
-                print("Thanks for remembering us to make this product supports for", typeOfFile)
+        else:
+            print("Thanks for remembering us to make this product supports for", typeOfFile)
 
 
 if __name__ == '__main__':
